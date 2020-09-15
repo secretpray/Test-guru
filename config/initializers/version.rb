651cc4c846c9a0452780ae5d11c39f31056c5b01
secretpray <a.p.ruban@gmail.com>
2020-09-15 10:33:19 +0300

    Lesson 2

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -3,18 +3,6 @@ module TestGuru
   VERSION = '0.1.0'.freeze
 
   def self.version
-    "v#{self.read_version}"
+    VERSION
   end
-
-def self.read_version
-  begin
-    File.read 'VERSION'
-  rescue
-    raise "VERSION file not found or unreadable."
-  end
-end
-
-  # def self.version
-  #   VERSION
-  # end
 end
