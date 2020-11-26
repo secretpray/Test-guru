b946d0f0551bf1216586c86bb706b639d917b4fa
secretpray <Renovatio100>
2020-11-26 09:13:17 +0200

    Lesson 16 Gmail fix#4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://guru-test-2020.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    addresss:              'smtp.gmail.com',
+    address:                'smtp.gmail.com',
     port:                   587,
     user_name:              ENV['SMTP_USERNAME'],
     password:               ENV['SMTP_PASSWORD'],
